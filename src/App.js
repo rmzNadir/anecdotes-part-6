2d6e15a6c9b5b51f6050390e8ed0c6d799511653
rmzNadir <dgzrz99@gmail.com>
2021-01-09 01:22:56 -0600

    Exercise 6.6 - 6.7

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { addVote } from './reducers/anecdoteReducer';
-import NewAnecdote from './components/NewAnecdote';
+import AnecdoteForm from './components/AnecdoteForm';
 
 const App = () => {
   const anecdotes = useSelector((state) => state);
@@ -23,7 +23,7 @@ const App = () => {
           </div>
         </div>
       ))}
-      <NewAnecdote />
+      <AnecdoteForm />
     </div>
   );
 };
