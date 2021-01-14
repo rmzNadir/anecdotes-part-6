9dd8fc9fff5a3b200b4507e07da71fd8ac1f81f4
rmzNadir <dgzrz99@gmail.com>
2021-01-13 19:22:40 -0600

    Exercise 6.13

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { initializeAnecdotes } from './reducers/anecdoteReducer';
+import anecdotesService from './services/anecdotes';
+import { useDispatch } from 'react-redux';
+
+/* Componentes */
 import AnecdoteForm from './components/AnecdoteForm';
 import AnecdoteList from './components/AnecdoteList';
 import Notification from './components/Notification';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const { getAll } = anecdotesService;
+
+    getAll().then((anecdotes) => dispatch(initializeAnecdotes(anecdotes)));
+  }, [dispatch]);
+
   return (
     <div>
       <Notification />
--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -1,26 +1,7 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-];
-
-const getId = () => (100000 * Math.random()).toFixed(0);
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0,
-  };
-};
-
-const initialState = anecdotesAtStart.map(asObject);
-
-const anecdotesReducer = (state = initialState, action) => {
+const anecdotesReducer = (state = [], action) => {
   switch (action.type) {
+    case 'INIT_ANECDOTES':
+      return action.data;
     case 'ADD_VOTE':
       const id = action.data.id;
 
@@ -42,6 +23,13 @@ const anecdotesReducer = (state = initialState, action) => {
   }
 };
 
+export const initializeAnecdotes = (anecdotes) => {
+  return {
+    type: 'INIT_ANECDOTES',
+    data: anecdotes,
+  };
+};
+
 export const addVote = (id) => {
   return {
     type: 'ADD_VOTE',
@@ -49,14 +37,10 @@ export const addVote = (id) => {
   };
 };
 
-export const createAnecdote = (content) => {
+export const createAnecdote = (anecdote) => {
   return {
     type: 'NEW_ANECDOTE',
-    data: {
-      content,
-      id: getId(),
-      votes: 0,
-    },
+    data: anecdote,
   };
 };
 
